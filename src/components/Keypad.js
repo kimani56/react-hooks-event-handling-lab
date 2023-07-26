b9c3442481981789d005ee4b5e0a8e60b89d4160
gift kimani <gift.kimani@students.moringaschool.com>
2023-07-26 16:05:04 +0300

    complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
+    const changeFired = (e) => {
+       
+        console.log('Entering password...');
+    };
+
     return (
-        <div></div>
-    )
+        <div>
+            <label htmlFor='passwordInput'>Enter Password</label>
+<input type ="password" id='passwordInput' onChange ={changeFired}/>
+
+        </div>
+    );
 }
 
 export default Keypad;
